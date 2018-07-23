8cbb58d6ac01e267a4488b7d5a87c7c151d32918
frogsicle <a7denton@gmail.com>
2018-07-23 18:38:17 +0200

    and now for labelled fieldspec data, tested so far as datagen

--- fieldspec_problems.py
+++ fieldspec_problems.py
@@ -6,6 +6,7 @@ from genic_problems import serialize_from_numpy, DataExistsError
 
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import os
 
 ### FieldSpec problems ###
@@ -32,7 +33,7 @@ class FieldSpecProblem(problem.Problem):
     def num_shards(self):
         return 20
 
-    # stuff I should probably but in a parent class for all my problems
+    # stuff I should probably put in a parent class for all my problems
     @property
     def num_shards_dev_test(self):
         return 4
@@ -44,18 +45,24 @@ class FieldSpecProblem(problem.Problem):
     def label_shape(self):
         return [self.number_wavelengths, 1]
 
-    # data serializing
-    def generate_data(self, data_dir, tmp_dir='/tmp/', task_id=-1):
+    def setup_file_paths(self, data_dir):
         out_files = {'train': self.training_filepaths(data_dir=data_dir, num_shards=self.num_shards,
                                                       shuffled=not self.shuffle_me),
                      'dev': self.dev_filepaths(data_dir=data_dir, num_shards=self.num_shards_dev_test,
-                                               shuffled=not self.shuffle_me)}
+                                               shuffled=not self.shuffle_me),
+                     'test': self.test_filepaths(data_dir=data_dir, num_shards=self.num_shards,
+                                                 shuffled=not self.shuffle_me)}
         # stop if data already exists
         finished = self.training_filepaths(data_dir=data_dir, num_shards=self.num_shards, shuffled=True)
         finished += self.dev_filepaths(data_dir=data_dir, num_shards=self.num_shards_dev_test, shuffled=True)
         for fin in finished:
             if os.path.exists(fin):
                 raise DataExistsError("data already exists at (at least): {}, won't overwrite".format(fin))
+        return out_files
+
+    # data serializing
+    def generate_data(self, data_dir, tmp_dir='/tmp/', task_id=-1):
+        out_files = self.setup_file_paths(data_dir)
 
         for dat_set in ['train', 'dev']:
             files_in = os.listdir(self.directory_in)
@@ -108,6 +115,132 @@ class FieldSpecProblem(problem.Problem):
         except KeyError:
             labels = example['inputs']
         return features, labels
+
+
+class FieldSpecLabelledProblem(problem.Problem):
+    """Problem for playing with FieldSpec reflectance data when it has labels
+
+    Takes input as folder of csvs with all labels to the left, of all reflectance data
+    The column 'sets' should contain xval/test/train designations"""
+
+    # location and shape of input data
+    @property
+    def file_in(self):
+        raise NotImplementedError
+
+    @property
+    def number_labels(self):
+        raise NotImplementedError
+
+    def get_label_columns(self, df):
+        # this should be code to get from a pandas dataframe the actual columns for the labels of interest
+        raise NotImplementedError
+
+    def get_one_label(self, labels, i):
+        raise NotImplementedError  # todo, tmp hack to get around subsetting in changing dimensionality
+
+    @property
+    def number_wavelengths(self):
+        raise NotImplementedError
+
+    # other stuff
+    @property
+    def shuffle_me(self):
+        return True
+
+    @property
+    def num_shards(self):
+        return 20
+
+    # stuff I should probably put in a parent class for all my problems
+    @property
+    def num_shards_dev_test(self):
+        return 4
+
+    def make_meta(self, data_dir):
+        return {'labels_shape': self.label_shape}
+
+    @property
+    def label_shape(self):
+        return [self.number_wavelengths, self.number_labels]
+
+    def setup_file_paths(self, data_dir):
+        out_files = {'train': self.training_filepaths(data_dir=data_dir, num_shards=self.num_shards,
+                                                      shuffled=not self.shuffle_me),
+                     'dev': self.dev_filepaths(data_dir=data_dir, num_shards=self.num_shards_dev_test,
+                                               shuffled=not self.shuffle_me),
+                     'test': self.test_filepaths(data_dir=data_dir, num_shards=self.num_shards,
+                                                 shuffled=not self.shuffle_me)}
+        # stop if data already exists
+        finished = self.training_filepaths(data_dir=data_dir, num_shards=self.num_shards, shuffled=True)
+        finished += self.dev_filepaths(data_dir=data_dir, num_shards=self.num_shards_dev_test, shuffled=True)
+        for fin in finished:
+            if os.path.exists(fin):
+                raise DataExistsError("data already exists at (at least): {}, won't overwrite".format(fin))
+        return out_files
+
+
+    # data serializing
+    def generate_data(self, data_dir, tmp_dir='/tmp/', task_id=-1):
+        out_files = self.setup_file_paths(data_dir)
+        print(out_files)
+        name_fix = {'xval': 'dev', 'test': 'test', 'train': 'train'}
+
+        df = pd.read_csv(self.file_in)
+
+        for dat_set in name_fix.keys():
+            targ_df = df[df['sets'] == dat_set]
+            x_df = targ_df.iloc[:, (targ_df.shape[1] - self.number_wavelengths):]
+            x_df = np.array(x_df)
+            y_df = self.get_label_columns(targ_df)
+            print(targ_df['sets'])
+            print(targ_df.shape)
+            self.generate_dataset(reflectance=x_df, labels=y_df, outfiles=out_files[name_fix[dat_set]])
+        if self.shuffle_me:
+            generator_utils.shuffle_dataset(out_files['train'] + out_files['dev'])
+
+    def generate_dataset(self, reflectance, labels, outfiles):
+        generator_utils.generate_files(self.dataset_generator(reflectance, labels), outfiles)
+
+    def dataset_generator(self, reflectance, labels):
+        for i in range(reflectance.shape[0]):
+            print(reflectance.shape[0])
+            ref_flat = serialize_from_numpy(reflectance[i, :], float)
+            lab_flat = serialize_from_numpy(self.get_one_label(labels, i), float)
+            yield {'inputs': ref_flat, 'targets': lab_flat}
+
+    def example_reading_spec(self):
+        data_fields = {
+            'inputs': tf.FixedLenFeature([self.number_wavelengths], tf.float32),
+        }
+        return data_fields, None  # idk what the None is a placeholder for in real t2t land
+
+    def preprocess_example(self, example, mode, unused_hparams):
+        example['inputs'] = tf.reshape(example['inputs'], [self.number_wavelengths, 1])
+        # data is legit 1D this time, so not much to change
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            # todo, linear transform on amplitude?
+            # or perhaps addition of drifting baseline?
+            pass
+        return example
+
+    def parser(self, serialized_example, mode, meta_info=None):
+        if meta_info is None:
+            meta_info = {}
+
+        data_fields, _ = self.example_reading_spec()
+        features = tf.parse_single_example(serialized_example,
+                                           features=data_fields)
+        example = self.preprocess_example(features, mode, meta_info)
+        features = example['inputs']
+        try:  # not necessarily there as potentially have a lot of unlabelled data
+            labels = example['targets']
+        except KeyError:
+            labels = example['inputs']
+        return features, labels
+
+    # todo, needs it's own import
+    # todo, an easy way to make subclasses to indicate from same file, which label to use
 
 
 def merge_if_wavelengths_shared(data_in):
--- fieldspec_problems_subclasses.py
+++ fieldspec_problems_subclasses.py
@@ -1,4 +1,5 @@
-from fieldspec_problems import FieldSpecProblem
+import numpy as np
+from fieldspec_problems import FieldSpecProblem, FieldSpecLabelledProblem
 from tensor2tensor.utils import registry
 
 @registry.register_problem
@@ -12,3 +13,23 @@ class FieldSpecProblemTest(FieldSpecProblem):
     def number_wavelengths(self):
         return 2151
 
+
+@registry.register_problem
+class FieldSpecProblemLabelledTest(FieldSpecLabelledProblem):
+    @property
+    def file_in(self):
+        return 'labelled.csv'
+
+    @property
+    def number_wavelengths(self):
+        return 2151
+
+    def get_label_columns(self, df):
+        labels = np.array(df['Condition'])
+        labels[labels == 'control'] = 0
+        labels[labels == 'Drought'] = 1
+        labels = labels.reshape((labels.shape[0], 1))
+        return labels
+
+    def get_one_label(self, labels, i):
+        return labels[i]
